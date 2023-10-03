601ad95e55ead7b6778130e9e6f3ee25b262267a
Thafsal <thafsalthafsu@gmail.com>
2023-10-03 18:14:06 +0530

    project completed front-end and backend as well

--- controller/task.js
+++ controller/task.js
@@ -1,5 +1,7 @@
 const Task = require("../models/task");
 
+
+//Get all the tasks
 const getallTasks = async (req, res) => {
   try {
     const tasks = await Task.find({})
@@ -8,9 +10,20 @@ const getallTasks = async (req, res) => {
     res.status(500).json({msg:error.errors.name.message})
   }
 };
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+//Get a task
+const getTask = async(req, res) => {
+ try {
+  const {id:taskID} = req.params
+  const task = await Task.findOne({_id:taskID})
+ if(!task){
+  return  res.status(404).json({msg : `No task with id ${taskID}`})
+ }
+ res.status(200).json({task})
+ } catch (error) {
+  res.status(500).json({msg:error.errors.name.message})
+ }
 };
+//create Task
 const createTasks = async (req, res) => {
   try {
     const task = await Task.create(req.body);
@@ -19,11 +32,37 @@ const createTasks = async (req, res) => {
     res.status(500).json({msg:error.errors.name.message})
   }
 };
-const updateTasks = (req, res) => {
-  res.send("update task");
+
+//Update the task
+const updateTasks = async(req, res) => {
+  try {
+    const {id:taskID} =req.params
+    
+    const task = await Task.findOneAndUpdate({_id:taskID} ,req.body,{
+      new:true,
+      runValidators:true,
+    })
+    if(!task){
+      res.status(404).json({msg:`No item with the id ${taskID}`})
+    }
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({msg:error.errors.name.message})
+  }
 };
-const deleteTasks = (req, res) => {
-  res.send("delete a task");
+
+//Delete a  task
+const deleteTasks = async(req, res) => {
+  try {
+    const {id:taskID} = req.params
+    const task = await Task.findOneAndDelete({_id:taskID})
+    if(!task){
+      return res.status(404).json({msg:`No task is found for the id${taskID}`})
+    }
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({msg:error.errors.name.message})
+  }
 };
 
 module.exports = {
